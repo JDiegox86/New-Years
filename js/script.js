2bec4f20cb362a14710c324c6bfb5e8ea99c6616
Juan Diego Chura Vallejos <juan.chura@tecsup.edu.pe>
2022-12-30 13:42:05 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const roulette = document.querySelector("#roulette");
+const spinButton = document.querySelector("#spin");
+const resetButton = document.querySelector("#reset");
+
+const maxSpins = 10;
+const minSpins = 1;
+
+const maxDegreens = 360;
+const minDegreens = 1;
+
+const getRandomNumber = (min, max) => {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+spinButton.addEventListener("click", () => {
+
+    const spins = getRandomNumber(minSpins, maxSpins);
+    const degrees = getRandomNumber(minDegreens, maxDegreens);
+
+    const fullSpins = (spins - 1) * 360;
+    const spin = fullSpins + degrees;
+
+    const animationTime = spins;
+
+    roulette.style.transform = `rotate(${spin}deg)`;
+    roulette.style.transitionDuration = `${animationTime}s`;
+
+    spinButton.style.display = "none";
+    resetButton.style.display = "inline-block"
+});
+
+resetButton.addEventListener("click", () => {
+    roulette.style.transform = `rotate(0deg)`;
+    roulette.style.transitionDuration = `2s`;
+    spinButton.style.display = `inline-block`;
+    resetButton.style.display = `none`;
+});
